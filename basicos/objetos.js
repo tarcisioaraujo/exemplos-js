ff1fb042d3c38cd4beb9f4497d2a56a55eb0c158
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2021-04-08 19:04:37 -0300

    Primeiro commit do projeto contendo exemplos de arrays, booleanos e objetos

---
+++ basicos/objetos.js
@@ -0,0 +1,13 @@
+const prod1 = {}
+prod1.nome = 'Smartphone'
+prod1.preco = 1999.99
+prod1['Desconto Legal'] = 0.75 // evitar atributos com espaço
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Blusa',
+    preco: 81.90
+}
+
+console.log(prod2)
