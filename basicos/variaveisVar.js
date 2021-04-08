57a3ae2cc6dff0e2af34a36ab0829f701cd80aee
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2021-04-08 20:27:29 -0300

    Realiza testes de escopo com variáveis do tipo var

---
+++ basicos/variaveisVar.js
@@ -0,0 +1,20 @@
+{ 
+    { 
+        { 
+            { 
+                var sera = 'Sera?'
+                console.log(sera)
+            }
+        }
+    }
+}
+
+console.log(sera)
+
+function teste() {
+    var local = 321
+    console.log(local)
+}
+
+teste()
+console.log(local)
