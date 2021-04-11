f5391088654d4b79cc90817f66a0f3d0272ec841
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2021-04-11 10:43:34 -0300

    Aplicando bind em uma função

---
+++ funções/bind.js
@@ -0,0 +1,13 @@
+const pessoa = {
+    saudacao: 'Olá, tudo bem?',
+    falar() {
+        console.log(this.saudacao)
+    }
+}
+
+pessoa.falar()
+const falar = pessoa.falar
+falar() // conflito entre paradigma funcional e OO
+
+const falarDePessoa = pessoa.falar.bind(pessoa)
+falarDePessoa()
