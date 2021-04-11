a0a9f55d1ff997ca12552987ca76f207e5f19971
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2021-04-11 10:55:40 -0300

    Atualiza testes com bind em função

--- funções/bind.js
+++ funções/bind.js
@@ -11,3 +11,15 @@ falar() // conflito entre paradigma funcional e OO
 
 const falarDePessoa = pessoa.falar.bind(pessoa)
 falarDePessoa()
+
+function Pessoa() {
+    this.idade = 0
+
+    const self = this
+    setInterval(function() {
+        self.idade++
+        console.log(self.idade)
+    }/*.bind(this)*/, 1000)
+}
+
+new Pessoa
