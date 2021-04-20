7e8170b7c3756347847e57446e6aea51d4f6db07
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2021-04-20 15:29:08 -0300

    Exemplo de funcão factory com parâmetros

--- funcoes/factory.js
+++ funcoes/factory.js
@@ -7,3 +7,16 @@ function criarPessoa() {
 }
 
 console.log(criarPessoa())
+
+//-------------------------------------------------------
+
+function criarProduto(nome, preco) {
+    return {
+        nome,
+        preco,
+        desconto: 0.1
+    }
+}
+
+console.log(criarProduto('Notebook', 2199.49))
+console.log(criarProduto('iPad', 1199.49))
