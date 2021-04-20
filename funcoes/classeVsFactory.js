ec3916fa76b57b93fdfbf9b9e2b80dfdbc07959d
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2021-04-20 15:56:01 -0300

    Transforma classe em uma função construtora

--- funcoes/classeVsFactory.js
+++ funcoes/classeVsFactory.js
@@ -19,3 +19,15 @@ const criarPessoa = nome => {
 
 const p2 = criarPessoa('João')
 p2.falar()
+
+// function Pessoa(nome) {
+//     this.nome = nome
+    
+//     this.falar = function() {
+//         console.log(`Meu nome é ${this.nome}`)
+//     }
+// }
+
+// const p1 = new Pessoa('João')
+// p1.falar()
+// console.log(p1.nome)
