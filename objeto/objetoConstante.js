0b7e70544653d933f7c0f2b36067162ce2b7a8d1
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2021-04-20 18:26:02 -0300

    Criando objetos contantes

---
+++ objeto/objetoConstante.js
@@ -0,0 +1,21 @@
+// pessoa (variável) -> 123 (endereço de memória) -> {...} (valor do objeto)
+const pessoa = { nome: 'Joao' }
+pessoa.nome = 'Pedro'
+console.log(pessoa)
+
+// pessoa -> 456 -> {...}
+// pessoa = { nome: 'Ana' }
+
+//Congela o objeto. Os atributos n podem ser modificados, deletados ou criados
+Object.freeze(pessoa)
+
+pessoa.nome = 'Maria'
+pessoa.end = 'Rua ABC'
+delete pessoa.nome
+
+console.log(pessoa.nome)
+console.log(pessoa)
+
+const pessoaConstante = Object.freeze({ nome: 'Joao' })
+pessoaConstante.nome = 'Maria'
+console.log(pessoaConstante)
