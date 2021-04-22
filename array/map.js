487bb20a7c390f6448d263348c5e619ff3153dc1
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2021-04-22 17:42:36 -0300

    Mais testes com a função map em arrays

--- array/map.js
+++ array/map.js
@@ -13,3 +13,20 @@ const paraDinheiro = e => `R$ ${parseFloat(e).toFixed(2).replace('.', ',')}`
 
 resultado = nums.map(soma10).map(triplo).map(paraDinheiro)
 console.log(resultado)
+
+//--------------------------------------------
+
+const carrinho = [
+    '{ "nome": "Borracha", "preco": 3.45 }',
+    '{ "nome": "Caderno", "preco": 13.90 }',
+    '{ "nome": "Kit de Lapis", "preco": 41.22 }',
+    '{ "nome": "Caneta", "preco": 7.50 }'
+]
+
+// Retornar um array apenas com os preços
+
+const paraObjeto = json => JSON.parse(json)
+const apenasPreco = produto => produto.preco
+
+const resultadoArrayPrecos = carrinho.map(paraObjeto).map(apenasPreco)
+console.log(resultadoArrayPrecos)
