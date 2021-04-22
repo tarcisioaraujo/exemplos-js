7dfb662ef9fe6f7e4237b60fe8b2181a690a5920
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2021-04-22 15:35:37 -0300

    Evitando modificações em objetos

---
+++ objeto/evitandoModificacoes.js
@@ -0,0 +1,22 @@
+// Object.preventExtensions (Não permite adicionar novos atributos)
+const produto = Object.preventExtensions({
+    nome: 'Qualquer', preco: 1.99, tag: 'promoção'
+})
+console.log('Extensível:', Object.isExtensible(produto))
+
+produto.nome = 'Borracha'
+produto.descricao = 'Borracha escolar branca'
+delete produto.tag
+console.log(produto)
+
+// Object.seal (Não permite adicionar nem remover atributos)
+const pessoa = { nome: 'Juliana', idade: 35 }
+Object.seal(pessoa)
+console.log('Selado:', Object.isSealed(pessoa))
+
+pessoa.sobrenome = 'Silva'
+delete pessoa.nome
+pessoa.idade = 29
+console.log(pessoa)
+
+//Object.freeze(obj) valores constantes e selados
