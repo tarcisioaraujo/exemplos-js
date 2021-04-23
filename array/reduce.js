ae536e7c6cee46d4b622b5b69d44658e9c88b05d
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2021-04-23 11:06:05 -0300

    Mais alguns testes com reduce

--- array/reduce.js
+++ array/reduce.js
@@ -12,3 +12,21 @@ const resultado = alunos.map(a => a.nota).reduce(function(acumulador, atual) {
 }, 0)
 
 console.log(resultado)
+
+//--------------------------------
+
+
+const alunosBolsistas = [
+    { nome: 'João', nota: 7.3, bolsista: false },
+    { nome: 'Maria', nota: 9.2, bolsista: true },
+    { nome: 'Pedro', nota: 9.8, bolsista: false },
+    { nome: 'Ana', nota: 8.7, bolsista: true }
+]
+
+// Desafio 1: Todos os alunos são bolsista?
+const todosBolsistas = (resultado, bolsista) => resultado && bolsista
+console.log(alunosBolsistas.map(a => a.bolsista).reduce(todosBolsistas))
+
+// Desafio 2: Algum aluno é bolsista?
+const algumBolsista = (resultado, bolsista) => resultado || bolsista
+console.log(alunosBolsistas.map(a => a.bolsista).reduce(algumBolsista))
