832b5133a2a398a541ebf9c6c3a5df7824967fbd
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2021-08-02 18:47:26 -0300

    Tenta carregar arquivos de imagem

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,8 +1,8 @@
 const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
     mode: modoDev ? 'development' : 'production',
@@ -12,28 +12,40 @@ module.exports = {
         path: __dirname + '/public'
     },
     optimization: {
-        minimizer: [
-            new UglifyJsPlugin({
-                cache: true,
-                parallel: true
-            }),
+        minimizer: [            
             new OptimizeCSSAssetsPlugin({})
         ]
     },
     plugins: [
         new MiniCssExtractPlugin({
             filename: "estilo.css"
+        }), 
+        new TerserPlugin({
+            parallel: true,
+            terserOptions: {
+                ecma: 6,
+            },
         })
     ],
     module: {
         rules: [{
             test: /\.s?[ac]ss$/,
-            use: [    
-                MiniCssExtractPlugin.loader,            
+            use: [
+                MiniCssExtractPlugin.loader,
                 // 'style-loader', // Adiciona CSS a DOM injetando a tag <style>
                 'css-loader', // interpreta @import, url()...
                 'sass-loader',
             ]
+        }, {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: [
+                {
+                  loader: 'file-loader',
+                  options: {
+                    esModule: false
+                  }
+                }
+              ]
         }]
     }
 }
