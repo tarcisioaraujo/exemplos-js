2eab56e824f4bddf28366cd617f4deadc1c6d28c
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2022-05-26 19:04:38 -0300

    Multiplos componentes no mesmo arquivo.

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 // import Primeiro from './componentes/Primeiro'
-import Bomdia from './componentes/BomDia'
+// import Bomdia from './componentes/BomDia'
+import Multiplos, { BoaNoite } from './componentes/Multiplos'
 
 
 ReactDOM.render(
-    <Bomdia nome="Tarcisio" />   
+    // <Bomdia nome="Beltrano" />   
+    <div>        
+        <Multiplos.BoaTarde nome="Alice" />
+        <BoaNoite nome="Joaquim" />
+    </div>    
 , document.getElementById('root'))
