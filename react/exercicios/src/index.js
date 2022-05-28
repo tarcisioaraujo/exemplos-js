98dd3e562b1faeced443358e274715649c088745
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2022-05-28 18:32:58 -0300

    Criando um componente de classe simples.

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom'
 
 // import Primeiro from './componentes/Primeiro'
 // import Bomdia from './componentes/BomDia'
-import Multiplos, { BoaNoite } from './componentes/Multiplos'
+// import Multiplos, { BoaNoite } from './componentes/Multiplos'
+import Saudacao from './componentes/Saudacao'
 
 
 ReactDOM.render(
     // <Bomdia nome="Beltrano" />   
     <div>        
-        <Multiplos.BoaTarde nome="Alice" />
-        <BoaNoite nome="Joaquim" />
+        {/* <Multiplos.BoaTarde nome="Alice" />
+        <BoaNoite nome="Joaquim" /> */}
+        <Saudacao tipo="Bom dia" nome="João"/>
     </div>    
 , document.getElementById('root'))
