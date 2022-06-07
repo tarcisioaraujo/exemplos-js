7a57c589f43e438dfe17558577bfcc02d74f67d7
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2022-06-07 20:20:11 -0300

    Implementa função de addDigit.

--- react/calculadora/src/main/Calculator.jsx
+++ react/calculadora/src/main/Calculator.jsx
@@ -33,7 +33,24 @@ export default class Calculator extends Component {
     }
 
     addDigit(n) {
-        console.log('addDigit', n);
+        if (n === '.' && this.state.displayValue.includes('.')) {
+            return
+        }
+
+        const clearDisplay = this.state.displayValue === '0'
+            || this.state.clearDisplay
+        const currentValue = clearDisplay ? '' : this.state.displayValue
+        const displayValue = currentValue + n
+        console.log('displayValue', displayValue);
+        this.setState({ displayValue, clearDisplay: false })
+
+        if (n !== '.') {
+            const i = this.state.current
+            const newValue = parseFloat(displayValue)
+            const values = [...this.state.values]
+            values[i] = newValue
+            this.setState({ values })
+        }
     }
 
     render() {
