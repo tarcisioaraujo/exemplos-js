c113badb6b4b10af5699d2291182f4e34070a265
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2022-06-21 19:46:12 -0300

    Desenvolve componente menu de navegação lateral.

--- react/crud/frontend/src/components/template/Nav.jsx
+++ react/crud/frontend/src/components/template/Nav.jsx
@@ -2,6 +2,13 @@ import './Nav.css';
 import React from 'react';
 
 export default prosp =>
-    <aside className='menu-area'>
-        Navegação
+    <aside className="menu-area">
+        <nav className="menu">            
+            <a href="#/">
+                <i className="fa fa-home"></i> Início
+            </a>
+            <a href="#/users">
+                <i className="fa fa-users"></i> Usuários
+            </a>
+        </nav>
     </aside>
